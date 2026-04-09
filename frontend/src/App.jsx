efc69c18d4d685bf679f8eb5681b5b23f03a887d
Greg T. <greg.telakis@gmail.com>
2026-04-09 22:17:52 +0300

    feat: initial project setup with Vite, React and Tailwind v4

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "./assets/vite.svg";
+// import heroImg from "./assets/hero.png";
+import "./index.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1 className="bg-yellow-500 h-screen">Greg's Portfolio - coming soon</h1>
+    </div>
+  );
+}
+
+export default App;
